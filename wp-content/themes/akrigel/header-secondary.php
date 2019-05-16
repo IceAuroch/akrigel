123d4b1c89d769694158b23c44684f1abe91dbc8
MaksFisher <adriel.black666@gmail.com>
2019-05-16 15:51:00 +0300

    add back call

--- wp-content/themes/akrigel/header-secondary.php
+++ wp-content/themes/akrigel/header-secondary.php
@@ -20,6 +20,14 @@
     gtag('config', 'UA-140306206-1');
     </script>
 
+    <script type="text/javascript">
+    (function(d, w, s) {
+    var widgetHash = 'i4rsyqi14w0o5twg87tt', gcw = d.createElement(s); gcw.type = 'text/javascript'; gcw.async = true;
+    gcw.src = '//widgets.binotel.com/getcall/widgets/'+ widgetHash +'.js';
+    var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
+    })(document, window, 'script');
+    </script>
+
 
 </head>
 
