baaccd7391d649d78fd795d329216b729075ada7
MaksFisher <adriel.black666@gmail.com>
2019-05-16 11:57:57 +0300

    add google A

--- wp-content/themes/akrigel/header-secondary.php
+++ wp-content/themes/akrigel/header-secondary.php
@@ -9,6 +9,18 @@
     <?php wp_head(); ?>
     <link href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" rel="stylesheet"/>
 
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140306206-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-140306206-1');
+    </script>
+
+
 </head>
 
 <body>
